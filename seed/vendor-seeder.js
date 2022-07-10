e799d4c4799ee36dcca505fce36f9af30698a270
Eric Carlson <eric.carlson2972@gmail.com>
2022-07-10 06:26:21 -0400

    Add a folder for seeding data to the vendor model

---
+++ seed/vendor-seeder.js
@@ -0,0 +1,28 @@
+const Vendor = require("../models/Vendor");
+
+const vendors = [
+  new Vendor({
+    name: "Emma's Garden",
+    imagePath: "",
+    cloudinaryId: 5,
+    location: "42 Palmerston Blvd",
+  }),
+  new Vendor({
+    name: "Wendy's Raspberries",
+    imagePath: "",
+    cloudinaryId: 1,
+    location: "12 Mississauga Blvd",
+  }),
+  new Vendor({
+    name: "Eric's Awesome Garden",
+    imagePath: "",
+    cloudinaryId: 2,
+    location: "46 Lakeview Ave",
+  }),
+  new Vendor({
+    name: "Terry's Tomatoes",
+    imagePath: "",
+    cloudinaryId: 3,
+    location: "123 Elm St",
+  }),
+];
