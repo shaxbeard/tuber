95b0c3fd3070ebca4a0b44ec3e30190c98eda0cd
Eric Carlson <eric.carlson2972@gmail.com>
2022-09-27 06:13:25 -0400

    Add a seeder file to the project for the vendors

--- seed/vendor-seeder.js
+++ seed/vendor-seeder.js
@@ -1,28 +1,50 @@
 const Vendor = require("../models/Vendor");
 
+var mongoose = require("mongoose");
+
+mongoose.connect("localhost:2101/feed");
+
 const vendors = [
   new Vendor({
-    name: "Emma's Garden",
-    imagePath: "",
-    cloudinaryId: 5,
+    name: "Sweet Times Berry Patch",
+    imagePath:
+      "https://res.cloudinary.com/dewbhwqgu/image/upload/v1664243034/sweet-times-berry-atch_j64nam.jpg",
+    cloudinaryId: "sweet-times-berry-atch_j64nam",
     location: "42 Palmerston Blvd",
   }),
   new Vendor({
     name: "Wendy's Raspberries",
-    imagePath: "",
-    cloudinaryId: 1,
+    imagePath:
+      "https://res.cloudinary.com/dewbhwqgu/image/upload/v1664243034/raspberry-garden_qvdnaf.jpg",
+    cloudinaryId: "raspberry-garden_qvdnaf",
     location: "12 Mississauga Blvd",
   }),
   new Vendor({
-    name: "Eric's Awesome Garden",
-    imagePath: "",
-    cloudinaryId: 2,
-    location: "46 Lakeview Ave",
+    name: "Maeve's Rhubarb Realm",
+    imagePath:
+      "https://res.cloudinary.com/dewbhwqgu/image/upload/v1664243034/rhubarb-garden_dmihvz.jpg",
+    cloudinaryId: "rhubarb-garden_dmihvz",
+    location: "46 Cedarvale Ave",
   }),
   new Vendor({
-    name: "Terry's Tomatoes",
-    imagePath: "",
-    cloudinaryId: 3,
+    name: "Tatiana's Heirloom Tomatoes",
+    imagePath:
+      "https://res.cloudinary.com/dewbhwqgu/image/upload/v1664243034/heirloom-tomato-garden_lzlrry.jpg",
+    cloudinaryId: "heirloom-tomato-garden_lzlrry",
     location: "123 Elm St",
   }),
 ];
+
+var done = 0;
+for (var i = 0; i < vendors.length; i++) {
+  vendors[i].save(function (err, results) {
+    done++;
+    if (done === vendors.length) {
+      exit();
+    }
+  });
+}
+
+function exit() {
+  mongoose.disconnect();
+}
